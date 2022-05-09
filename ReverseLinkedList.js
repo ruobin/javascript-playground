4f0e9112f9b465a99bafdcaecff91dad9d73bbdd
robin <robin@carehealth.io>
2022-05-09 12:16:40 +0800

    add cases

---
+++ ReverseLinkedList.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    if (head === null || head.next === null) {
+        return head;
+    }
+
+    let prev = null;
+    let curr = head;
+    let next = null;
+
+    while (curr.next !== null) {
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    curr.next = prev;
+    return curr;
+};
---
+++ bitcoinProfilt.js
@@ -0,0 +1,72 @@
+const inputData = [
+    {
+        "date": 1,
+        "type": "buy",
+        "price": 25,
+        "amount": 5
+    }, {
+        "date": 2,
+        "type": "buy",
+        "price": 40,
+        "amount": 10
+    }, {
+        "date": 3,
+        "type": "sell",
+        "price": 42,
+        "amount": 7
+    }, {
+        "date": 4,
+        "type": "sell",
+        "price": 3,
+        "amount": 1
+    }, {
+        "date": 5,
+        "type": "buy",
+        "price": 25,
+        "amount": 5
+    }, {
+        "date": 6,
+        "type": "sell",
+        "price": 20,
+        "amount": 10
+    }
+];
+
+// [{"date":3,"gain_or_loss":89},{"date":4,"gain_or_loss":-37},{"date":6,"gain_or_loss":-155}]
+
+function buildTheData(inputData) {
+    let balance = 0;
+    let queue = [];
+    let results = [];
+    for (let i = 0; i < inputData.length; i++) {
+        if (inputData[i].type === "buy") {
+            queue.push({ amount: inputData[i].amount, price: inputData[i].price });
+        } else if (inputData[i].type === "sell") {
+            let profitOrLoss = 0;
+            let sellingAmount = inputData[i].amount;
+            const sellingPrice = inputData[i].price;
+            while (sellingAmount > 0) {
+                const previousSellingAmount = sellingAmount;
+                sellingAmount -= queue[0].amount;
+                
+                if (sellingAmount > 0) {
+                    profitOrLoss += (sellingPrice - queue[0].price) * queue[0].amount;
+                    queue.shift();
+                } else {
+                    queue[0].amount -= previousSellingAmount;
+                    profitOrLoss += (sellingPrice - queue[0].price) * previousSellingAmount;
+                }
+            }
+
+            results.push({
+                date: inputData[i].date,
+                gain_or_loss: profitOrLoss
+            });
+        }
+
+    }
+    return results;
+
+}
+
+console.log(JSON.stringify(buildTheData(inputData)));
